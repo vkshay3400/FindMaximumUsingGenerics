1ea075983ce2991b54d359eec4d39df4ed6ef9f1
vkshay3400 <akshayp2208@gmail.com>
2020-03-14 17:38:42 +0530

    Added: Given three integer when second maximun return maximum

--- src/test/java/FindMaximumProblemTest.java
+++ src/test/java/FindMaximumProblemTest.java
@@ -5,8 +5,14 @@ public class FindMaximumProblemTest {
     FindMaximumProblem findMaximum = new FindMaximumProblem();
 
     @Test
-        public void givenThreeIntegers_whenMaximumFirstNumber_ThenShouldReturnMaximum() {
+        public void givenThreeIntegers_WhenMaximumFirstNumber_ThenShouldReturnMaximum() {
             int maximumOfInteger = findMaximum.getMaximumOfInteger(10, 8, 9);
         Assert.assertEquals(10, maximumOfInteger);
+    }
+
+    @Test
+    public void givenThreeIntegers_WhenMaximumSecondNumber_ThenShouldReturnMaximum() {
+        int maximumOfInteger = findMaximum.getMaximumOfInteger(10, 12, 9);
+        Assert.assertEquals(12, maximumOfInteger);
     }
 }
